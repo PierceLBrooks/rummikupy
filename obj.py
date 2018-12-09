d8e12cc81429e89a124b6273ac2f839d4ba563c7
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2018-12-08 21:29:07 -0500

    initial commit

---
+++ obj.py
@@ -0,0 +1,9 @@
+
+# Author: Pierce Brooks & Jeff Busch
+
+class Object(object):
+	def __init__(self):
+		pass
+
+	def update(self, pygame, screen, deltaTime):
+		pass
---
+++ rummiku.py
@@ -0,0 +1,40 @@
+
+# Authors: Pierce Brooks, Amalie Brooks, & Jeff Busch
+
+import sys
+import obj
+import importlib
+
+def run(pygame, arguments):
+	resolution = (800, 600)
+	pygame.display.init()
+	screen = pygame.display.set_mode(resolution)
+	clock = pygame.time.Clock()
+	objects = []
+	objects.append(obj.Object())
+	
+	quit = False
+	while not (quit):
+		#update
+		deltaTime = float(clock.tick())/1000.0
+		for object in objects:
+			print(object)
+			object.update(pygame, screen, deltaTime)
+		
+		#input
+		for event in pygame.event.get():
+			if (event.type == pygame.QUIT):
+				quit = True
+		if (quit):
+			break
+		
+		#render
+		pygame.display.flip()
+	
+	pygame.display.quit()
+	return 0
+
+if (__name__ == "__main__"):
+	arguments = sys.argv
+	pygame = importlib.import_module("pygame")
+	sys.exit(run(pygame, arguments))
