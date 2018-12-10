253992ba9c99644601c9ccf62a83a0b1658724ab
Jeff B <pancakewagons@gmail.com>
2018-12-09 20:40:19 -0500

    Press N to make a new object. Cant move it though. Help

--- piece.py
+++ piece.py
@@ -6,22 +6,24 @@ import util
 import random
 
 class Piece(obj.Object):
-	def __init__(self, position, imageX, imageY, rect):
+	def __init__(self, position):
 		self.position = position
 		self.image = None
-		self.imageX = imageX
-		self.imageY = imageY
-		self.rect = rect
+		self.imageX = None
+		self.imageY = None
+		self.rect = None
 		
 	def update(self, pygame, screen, background, deltaTime):
 		if (self.image == None):
 			self.image = util.loadImage(pygame, "piece.png")
+		
+		#Not sure if this is where this should be but it seems to work.
 		self.imageX = self.image.get_width()
 		self.imageY = self.image.get_height()
 		self.rect = pygame.Rect(self.position[0], self.position[1], self.imageX, self.imageY)
 
-
 		# range = 500
 		# self.position[0] += int(deltaTime*float(random.randint(-range, range)))
 		# self.position[1] += int(deltaTime*float(random.randint(-range, range)))
+		
 		background.blit(self.image, tuple(self.position))
--- rummiku.py
+++ rummiku.py
@@ -14,9 +14,11 @@ def run(pygame, arguments):
 	background = background.convert()
 	background.fill((0, 0, 0))
 	objects = []
-	objects.append(piece.Piece([resolution[0]/2, resolution[1]/2], None, None, None))
-	grid = g.makeGrid(pygame, resolution[0], resolution[1], 50, 50)
+	objects.append(piece.Piece([resolution[0]/2, resolution[1]/2]))
+	#grid = g.makeGrid(pygame, resolution[0], resolution[1], 40, 40)
 	clock = pygame.time.Clock()
+	
+	# Is it ok to have this here?
 	rectangle_draging = False
 	
 	quit = False
@@ -24,7 +26,7 @@ def run(pygame, arguments):
 		#update
 		background.fill((0, 0, 0))
 		deltaTime = float(clock.tick())/1000.0
-		background.blit(grid, (0,0))
+		#background.blit(grid, (0,0))
 		for object in objects:
 			object.update(pygame, screen, background, deltaTime)
 		screen.blit(background, (0, 0))
@@ -33,25 +35,32 @@ def run(pygame, arguments):
 		for event in pygame.event.get():
 			if (event.type == pygame.QUIT):
 				quit = True
-		if (quit):
-			break
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			if event.button == 1:            
-				if object.rect.collidepoint(event.pos):
-					rectangle_draging = True
+			if (quit):
+				break
+		
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_n:
+					objects.append(piece.Piece([resolution[0]/2, resolution[1]/2]))
+					print(objects)
+
+		#I found this code on google. I modified it to work with our objects.
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:            
+					if object.rect.collidepoint(event.pos):
+						rectangle_draging = True
+						mouse_x, mouse_y = event.pos
+						offset_x = object.position[0] - mouse_x
+						offset_y = object.position[1] - mouse_y
+
+			elif event.type == pygame.MOUSEBUTTONUP:
+				if event.button == 1:            
+					rectangle_draging = False
+
+			elif event.type == pygame.MOUSEMOTION:
+				if rectangle_draging:
 					mouse_x, mouse_y = event.pos
-					offset_x = object.position[0] - mouse_x
-					offset_y = object.position[1] - mouse_y
-
-		elif event.type == pygame.MOUSEBUTTONUP:
-			if event.button == 1:            
-				rectangle_draging = False
-
-		elif event.type == pygame.MOUSEMOTION:
-			if rectangle_draging:
-				mouse_x, mouse_y = event.pos
-				object.position[0] = mouse_x + offset_x
-				object.position[1] = mouse_y + offset_y
+					object.position[0] = mouse_x + offset_x
+					object.position[1] = mouse_y + offset_y
 		
 		#render
 		pygame.display.flip()
