97c630c764a706062b46a5db2ae59654c5bf887f
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2019-01-05 13:49:18 -0500

    implemented scaling options for pieces

--- util.py
+++ util.py
@@ -30,3 +30,59 @@ def loadImage(pygame, name):
 		print(message)
 		raise SystemExit
 	return image
+	
+def addVector(vector, number):
+	result = []
+	for i in range(len(vector)):
+		result.append(vector[i]+number)
+	return result
+	
+def subtractVector(vector, number):
+	result = []
+	for i in range(len(vector)):
+		result.append(vector[i]-number)
+	return result
+	
+def multiplyVector(vector, number):
+	result = []
+	for i in range(len(vector)):
+		result.append(vector[i]*number)
+	return result
+	
+def divideVector(vector, number):
+	result = []
+	for i in range(len(vector)):
+		result.append(vector[i]/number)
+	return result
+
+def addVectors(vector, other):
+	result = []
+	if (len(vector) < len(other)):
+		return None
+	for i in range(len(vector)):
+		result.append(vector[i]+other[i])
+	return result
+	
+def subtractVectors(vector, other):
+	result = []
+	if (len(vector) < len(other)):
+		return None
+	for i in range(len(vector)):
+		result.append(vector[i]-other[i])
+	return result
+	
+def multiplyVectors(vector, other):
+	result = []
+	if (len(vector) < len(other)):
+		return None
+	for i in range(len(vector)):
+		result.append(vector[i]*other[i])
+	return result
+	
+def divideVectors(vector, other):
+	result = []
+	if (len(vector) < len(other)):
+		return None
+	for i in range(len(vector)):
+		result.append(vector[i]/other[i])
+	return result
